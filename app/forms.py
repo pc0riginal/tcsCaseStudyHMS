e538ec9fce9e3ae4d8c0bf567669c5e1106cb627
msm1998 <pc.pramukh99@gmail.com>
2020-07-21 18:01:03 +0530

    validation issued fixes

--- app/forms.py
+++ app/forms.py
@@ -10,13 +10,13 @@ class LoginForm(FlaskForm):
 beds = [('None','None'),('General','general'),('Semi Sharing','semi sharing'),('Single','single')]
 class AddPatient(FlaskForm):
     patientID = StringField(label="Patient ID(SSN ID)",validators=[DataRequired(),Regexp('^\\d{9}$',message="required 9 digit ID")])
-    name = StringField(label="Name",validators=[DataRequired(),Length(min=3)])
+    name = StringField(label="Name",validators=[DataRequired(),Length(min=3,max=25),Regexp("^[a-zA-Z .'-]+$",message="only alphabets are allowed")])
     age = IntegerField(label="Age",validators=[DataRequired(),NumberRange(min=10,max=100)])
     typeOfBed = SelectField(label="Bed Type",choices=beds,validators=[Required()])
     addressline1 = StringField('Address Line 1*',validators=[DataRequired(), Length(min=1, max=100)])
     addressline2 = StringField('Address Line 2',validators=[Length(max=100)])
-    city = StringField('City',validators=[DataRequired()])
-    state = StringField('State',validators=[DataRequired()])
+    city = StringField('City',validators=[DataRequired(),Length(max=20),Regexp("^[a-zA-Z]+$",message="only alphabets are allowed")])
+    state = StringField('State',validators=[DataRequired(),Length(max=20),Regexp("^[a-zA-Z]+$",message="only alphabets are allowed")])
     submit = SubmitField(label="Create")
     def validate_typeOfBed(self,field):
         if self.typeOfBed.data == "None":
