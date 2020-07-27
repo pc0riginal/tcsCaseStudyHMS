2356937774a1f7e95dc4792935acbfc57e8b8a7d
msm1998 <pc.pramukh99@gmail.com>
2020-07-27 17:05:32 +0530

    csrf token added to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
 from flask_pymongo import PyMongo
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 app.config["DEBUG"] = True
 app.config.from_object(Config)
 mongo = PyMongo(app)
